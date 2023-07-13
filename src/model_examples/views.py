f5e7c257cd66ad86c872521779a4bd320bfcb276
Omar <oromero@unsa.edu.pe>
2023-07-12 23:46:46 -0500

    Generando el PDF

---
+++ src/model_examples/views.py
@@ -0,0 +1,22 @@
+#from django.http import HttpResponse
+from django.http import HttpResponse
+from django.views.generic import View
+
+from django.template.loader import get_template
+
+from model_examples.utils import render_to_pdf
+
+
+class GeneratePDF(View):
+    def get(self, request, *args, **kwargs):
+        template = get_template('invoice.html')
+        context = {
+            'invoice_id': 123,
+            'customer_name': "Omar Romero",
+            'amount': 1399.99,
+            'today':"Today",
+        }
+        html = template.render(context)
+        pdf = render_to_pdf('invoice.html', context)
+        #return HttpResponse(pdf, content_type='aplication/pdf')
+        return pdf
